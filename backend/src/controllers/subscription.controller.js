f25f2abae68b6fabc2ac9fe0ea70a1190b7b214a
Siddhant Jain <sidjain88tx@gmail.com>
2024-12-28 02:34:51 -0600

    feat: implement subscription toggle functionality and enhance subscriber retrieval with validation and aggregation

--- backend/src/controllers/subscription.controller.js
+++ backend/src/controllers/subscription.controller.js
@@ -7,12 +7,96 @@ import asyncHandler from "../utils/asyncHandler.js";
 
 const toggleSubscription = asyncHandler(async (req, res) => {
   const { channelId } = req.params;
-  // TODO: toggle subscription
+
+  if (!isValidObjectId(channelId)) {
+    throw new ApiError(400, "Invalid channel ID");
+  }
+
+  const channel = await User.findById(channelId).lean();
+
+  if (!channel) {
+    throw new ApiError(404, `Channel with ID ${channelId} not found`);
+  }
+
+  const currentSubscription = await Subscription.findOne({
+    channel: channelId,
+    subscriber: req.user._id,
+  }).lean();
+
+  if (currentSubscription) {
+    await Subscription.findByIdAndDelete(currentSubscription._id);
+  } else {
+    await Subscription.create({
+      subscriber: req.user._id,
+      channel: channelId,
+    });
+  }
+
+  res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        "",
+        currentSubscription
+          ? "Unsubscribed from the channel successfully"
+          : "Subscribed to the channel successfully"
+      )
+    );
 });
 
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
   const { channelId } = req.params;
+
+  if (!isValidObjectId(channelId)) {
+    throw new ApiError(400, "Invalid channel ID");
+  }
+
+  const channel = await User.findById(channelId).lean();
+
+  if (!channel) {
+    throw new ApiError(404, `Channel with ID ${channelId} not found`);
+  }
+
+  const subscribers = await Subscription.aggregate([
+    {
+      $match: {
+        $and: [
+          { channel: new mongoose.Types.ObjectId(channelId) },
+          { subscriber: new mongoose.Types.ObjectId(req.user._id) },
+        ],
+      },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "subscriber",
+        foreignField: "_id",
+        as: "subscribers",
+        pipeline: [
+          {
+            $project: {
+              _id: 1,
+              name: 1,
+              email: 1,
+              username: 1,
+              avatar: 1,
+            },
+          },
+        ],
+      },
+    },
+    {
+      $unwind: "$subscribers",
+    },
+  ]);
+
+  res
+    .status(200)
+    .json(
+      new ApiResponse(200, subscribers, "Subscribers fetched successfully")
+    );
 });
 
 // controller to return channel list to which user has subscribed
