dfa849092ba4d1b4ff9bc462293351e4d2b0b963
Siddhant Jain <sidjain88tx@gmail.com>
2024-12-31 16:36:54 -0600

    feat: implement clustering for improved server performance and reliability

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,3 +1,5 @@
+import cluster from "cluster";
+import os from "os";
 import connectDB from "./db/db.js";
 import app from "./app.js";
 import dotenv from "dotenv";
@@ -6,16 +8,33 @@ dotenv.config({
   path: "./.env",
 });
 
-connectDB()
-  .then(() => {
-    app.on("error", (error) => {
-      console.error(error);
-    });
+const PORT = process.env.PORT || 8000;
 
-    app.listen(process.env.PORT || 8000, () => {
-      console.log(`Server is running on port ${process.env.PORT}`);
-    });
-  })
-  .catch((error) => {
-    console.error("MongoDB connection failed: ", error);
+if (cluster.isPrimary) {
+  const numCPUs = os.cpus().length;
+  console.log(`Primary process ${process.pid} is running`);
+  console.log(`Forking ${numCPUs} workers...`);
+
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(`Worker ${worker.process.pid} exited. Forking a new worker...`);
+    cluster.fork();
   });
+} else {
+  connectDB()
+    .then(() => {
+      app.on("error", (error) => {
+        console.error(error);
+      });
+
+      app.listen(PORT, () => {
+        console.log(`Worker ${process.pid} is listening on port ${PORT}`);
+      });
+    })
+    .catch((error) => {
+      console.error("MongoDB connection failed: ", error);
+    });
+}
