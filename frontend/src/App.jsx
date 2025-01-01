2e77ddfb35af87126a998522aa670f760a650772
Siddhant Jain <sidjain88tx@gmail.com>
2024-12-31 20:33:05 -0600

    feat: integrate Redux for authentication management and configure environment variables

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,13 @@
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { login, logout } from "./store/authSlice";
+
 function App() {
   return (
     <>
       <h1>Video Zone App</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/store/authSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  userData: null,
+  isAuthenticated: false,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    login: (state, action) => {
+      state.isAuthenticated = true;
+      state.userData = action.payload.userData;
+    },
+    logout: (state) => {
+      state.isAuthenticated = false;
+      state.userData = null;
+    },
+  },
+});
+
+export const { login, logout } = authSlice.actions;
+export default authSlice.reducer;
